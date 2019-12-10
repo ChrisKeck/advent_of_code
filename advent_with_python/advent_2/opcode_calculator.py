dbcdae6a52a65aaf8a7d5f70a57f14e2076d1528
kec <christian.keck@iso-gruppe.com>
2019-12-10 02:19:26 +0100

    solved exercises

--- advent_with_python/advent_2/opcode_calculator.py
+++ advent_with_python/advent_2/opcode_calculator.py
@@ -16,6 +16,45 @@ def group(lsttogrp, n):
     return zip(*[lsttogrp[i::n] for i in range(n)])
 
 
+def puzzle_two():
+    goal = 19690720
+
+    with open('..//input_2.txt') as file:
+        input = list(map(lambda a: int(a), file.read().split(',')))
+
+    found = False
+    for noun in range(100):
+        for verb in range(100):
+            memory = input[:]
+            memory[1] = noun
+            memory[2] = verb
+
+            opcode, index = int(input[0]), 0
+            while opcode != 99:
+                if opcode == 1:
+                    sum = memory[memory[index + 1]] + memory[memory[index + 2]]
+                    memory[memory[index + 3]] = sum
+                elif opcode == 2:
+                    product = memory[memory[index + 1]] * memory[memory[index + 2]]
+                    memory[memory[index + 3]] = product
+                elif opcode != 99:
+                    print('Something went wrong')
+
+                index += 4
+                opcode = memory[index]
+
+            output = memory[0]
+
+            if output == goal:
+                final_noun = noun
+                final_verb = verb
+                found = True
+                break
+        if found == True:
+            break
+    print(f'Puzzle 2 - {100 * final_noun + final_verb}')
+
+
 if __name__ == "__main__":
     lst = read_modules("../input_2.txt")
     grplst = group(lst.copy(), 4)
@@ -28,3 +67,4 @@ if __name__ == "__main__":
         else:
             break
     print(lst)
+    puzzle_two()
